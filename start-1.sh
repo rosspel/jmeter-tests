bb19c4cb251104cedb95feed0a2b8793bf6fda26
rosspel <104831997+rosspel@users.noreply.github.com>
2024-10-21 15:22:47 +0200

    Create start-1.sh

---
+++ start-1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd bin
+mkdir reports
+sh jmeter -n -t /opt/apache-jmeter-5.5/test-plan-1.jmx \
+  -JThreadNumber=10 \
+  -JRampUpPeriod=1 \
+  -Jiterations=10 \
+  -l results.csv \
+  -e \
+  -o /opt/apache-jmeter-5.5/bin/reports/
+
+#tail -f /dev/null
