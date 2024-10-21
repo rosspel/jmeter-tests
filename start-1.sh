8bf6a0bc2ba0b0fc7a3545f21629024963e86d6c
rosspel <104831997+rosspel@users.noreply.github.com>
2024-10-21 18:22:08 +0200

    Update start-1.sh

--- start-1.sh
+++ start-1.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Define variables
+BUCKET_NAME="ecs-results"
+FILE_PATH="/opt/apache-jmeter-5.5/results.txt"
+S3_KEY="results.txt"
+
 cd bin
 mkdir reports
 sh jmeter -n -t /opt/apache-jmeter-5.5/test-plan-1.jmx \
@@ -9,4 +15,14 @@ sh jmeter -n -t /opt/apache-jmeter-5.5/test-plan-1.jmx \
   -e \
   -o /opt/apache-jmeter-5.5/bin/reports/
 
+echo "Tests finished"
 #tail -f /dev/null
+echo "Copying results on S3 bucket"
+aws s3 cp "$FILE_PATH" "s3://$BUCKET_NAME/$S3_KEY"
+
+if [ $? -eq 0 ]; then
+  echo "File uploaded successfully to s3://$BUCKET_NAME/$S3_KEY"
+else
+  echo "File upload failed!"
+  exit 1
+fi
